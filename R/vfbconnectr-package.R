cf085d7d166643e0df937fd683f49cf0469765c7
Gregory Jefferis <jefferis@gmail.com>
2021-03-02 02:58:29 +0000

    package details

--- R/vfbconnectr-package.R
+++ R/vfbconnectr-package.R
@@ -1,6 +1,14 @@
 #' @keywords internal
 #' @seealso \code{\link{VfbConnect}}, \code{\link{vc_install}}
 #'
+#' @details vfbconnectr provides a simple R wrapper (via
+#'   \code{\link{reticulate}}) for the
+#'   \href{https://pypi.org/project/vfb-connect/}{VFB_connect Python library}
+#'   that enables programmatic access to the \url{https://virtualflybrain.org}
+#'   website and associated services. VFB_connect is developed by the core VFB
+#'   team.
+
+#'
 #' @examples
 #' \dontrun{
 #' # make a VfbConnect to access the Python module
